6cdece5079507806e6fa94ee697b85e0a375b8ba
kwakmh <kmh001114@naver.com>
2025-08-08 15:24:12 +0900

    내용수정

--- src/main/java/AIServlet.java
+++ src/main/java/AIServlet.java
@@ -1,3 +1,8 @@
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import com.google.genai.Client;
+import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -16,7 +21,17 @@ public class AIServlet extends HttpServlet { // [1]
     // 경로에 들어갔을 때 (GET) -> 그 때 호출될 기능
     @Override // [3]
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("data", "안녕하세요! 반갑습니다!"); // [6]
+        Dotenv dotenv = Dotenv.load(); // [7]
+        String apiKey = dotenv.get("GOOGLE_API_KEY");
+        // [8] -> resources/.env
+        Client client = Client.builder()
+                .apiKey(apiKey).build(); // [9]
+        // gemini-2.0-flash
+        String data = client.models.generateContent("gemini-2.0-flash",
+                        "오늘 저녁 메뉴 추천해줘. 결과만 작성해줘. 마크다운 혹은 꾸미는 문법 없이 평문으로. 100자 이내로 작성해줘.", null)
+                .text(); // text를 불러와줌. [10]
+//        req.setAttribute("data", "안녕하세요! 반갑습니다!"); // [6]
+        req.setAttribute("data", data); // [11]
 
         RequestDispatcher dispatcher = req.getRequestDispatcher(
                 "/WEB-INF/ai.jsp");
@@ -24,5 +39,3 @@ public class AIServlet extends HttpServlet { // [1]
         // WEB-INF -> ai.jsp [5]
     }
 }
-
-
